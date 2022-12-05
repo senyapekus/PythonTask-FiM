d77c0a56453d8f2ede92401dd82398bee04c9fc8
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-12-06 02:36:52 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from view import View
+from model import Model
+from controller import Controller
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        model = Model('прив')
+
+        view = View(self)
+
+        controller = Controller(model, view)
+
+        view.set_controller(controller)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
