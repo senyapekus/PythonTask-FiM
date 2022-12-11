4ee03197793dac91bf1b1ac5507574abe552a02a
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-12-11 18:12:54 +0500

    Create example.py

---
+++ py_program/example.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    hello = 3
+    if hello == "Hello world":
+        for i in range(0, 99):
+            print("Hello is hello world")
+        print("priv")
+    else:
+        print("Hello is not hello world")
+        hello -= 2
+    print("%s" % hello)
+def Function1(Arg1, Arg2, Arg3):
+    print("Arg1")
+def Function2():
+    print("Hello, I'm without arguments")
