2096e11e1d7ab159676f32f2cabdc13c25735b82
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-12-11 18:08:24 +0500

    Update model.py

--- model.py
+++ model.py
@@ -2,4 +2,10 @@ class Model:
     def __init__(self, text_file):
         self.text_file = text_file
 
+    @property
+    def text_file(self):
+        return self.__text_file
 
+    @text_file.setter
+    def text_file(self, value):
+        self.__text_file = value
