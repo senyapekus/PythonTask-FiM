aa8540d6bc4621d2148f5c5a0d8eb36b6037cedc
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-12-21 23:39:48 +0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,12 +1,11 @@
 import os
 import re
-
 from compiler import Compiler
 
 
 def interpret():
-    compiler = Compiler("test_fim.fimpp")
-    return compiler
+    compiler_file = Compiler("test_fim.fimpp")
+    return compiler_file
 
 
 def get_py_program():
@@ -76,8 +75,109 @@ def test_get_value_in_array():
         assert "%s%s = %s[%s]" % (interpret().get_count_tabs(),
                                   match2.group(1).replace(" ", ""),
                                   match2.group(2).replace(" ", ""),
-                                  int(match2.group(3).replace(" ", "")))
+                                  int(match2.group(3).replace(" ", ""))) in program
 
 
+def test_cycle_for():
+    program = get_py_program()
+
+    match = re.search(r'for i in range\(0, ([0-9]+)\):', program)
+
+    assert "%sfor i in range(0, %d):" % (interpret().get_count_tabs(),
+                                         int(match.group(1).replace(" ", ""))) in program
 
 
+def test_cycle_while():
+    program = get_py_program()
+
+    match = re.search(r'while (.*) (\W) ([0-9]+):', program)
+
+    assert "%s%s %s %s %s:" % (interpret().get_count_tabs(),
+                               "while",
+                               match.group(1).replace(" ", ""),
+                               match.group(2),
+                               match.group(3).replace(" ", "")) in program
+
+
+def test_changing_variables():
+    program = get_py_program()
+
+    match = re.search(r'(.*) (.*)= ([0-9]+)', program)
+
+    assert "%s%s %s= %d" % (interpret().get_count_tabs(),
+                            match.group(1).replace(" ", ""),
+                            match.group(2).replace(" ", ""),
+                            int(match.group(3).replace(" ", ""))) in program
+
+
+def test_condition():
+    program = get_py_program()
+
+    match = re.search(r'(.*) (.*) (\W) (.*):', program)
+    tabs = interpret().get_count_tabs()
+
+    assert "%s%s %s %s %s:" % (tabs,
+                               match.group(1).replace(" ", ""),
+                               match.group(2).replace(" ", ""),
+                               match.group(3).replace(" ", ""),
+                               match.group(4).replace(" ", "")) in program
+
+
+def test_has_breakpoint():
+    assert interpret().get_inf_breakpoint() is True
+
+
+def test_has_comment():
+    program = get_py_program()
+
+    assert "#" in program
+
+
+def test_explicit_cast():
+    program = get_py_program()
+
+    match = re.search(r"(.*) = (.*)\((.*)\)", program)
+
+    assert "%s%s = %s(%s)" % (interpret().get_count_tabs(),
+                              match.group(1).replace(" ", ""),
+                              match.group(2).replace(" ", ""),
+                              match.group(3).replace(" ", "")) in program
+
+
+def test_create_func_with_args():
+    program = get_py_program()
+
+    match = re.search(r"def (.*)\((.*)\):", program)
+    args = match.group(2).replace(" ", "").split(',')
+
+    assert "%sdef %s(%s):" % ("", match.group(1).replace(" ", ""),
+                              ", ".join(args)) in program
+
+
+def test_create_func_without_args():
+    program = get_py_program()
+
+    match = re.search(r"def (.*)\(\):", program)
+
+    assert "%sdef %s():" % ("", match.group(1).replace(" ", "")) in program
+
+
+def test_call_func_with_args():
+    program = get_py_program()
+
+    match = re.search(r"(.*) = (.*)\((.*)\)", program)
+    args = match.group(3)
+
+    assert "%s%s = %s(%s)" % (interpret().get_count_tabs(),
+                              match.group(1).replace(" ", ""),
+                              match.group(2).replace(" ", ""),
+                              args) in program
+
+
+def test_call_func_without_args():
+    program = get_py_program()
+
+    match = re.search(r"(.*)\(\)", program)
+
+    assert "%s%s()" % (interpret().get_count_tabs(),
+                       match.group(1).replace(" ", "")) in program
